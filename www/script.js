51ea4848321c1da50914b72642c365c0264f21bd
weirdo28fr-crypto <weirdo.28fr@gmail.com>
2026-06-29 23:47:06 +0530

    Add game start and choice handling functions

--- www/script.js
+++ www/script.js
@@ -1,3 +1,25 @@
-function startGame() {
-  alert("Game Started 🎮 Welcome!");
+let text = document.getElementById("text");
+
+function choice1() {
+  text.innerHTML = "Nice 😄... I knew you’d say yes!";
+  
+  setTimeout(() => {
+    text.innerHTML = "Okay... first surprise loading 🎁";
+  }, 2000);
+
+  setTimeout(() => {
+    text.innerHTML = "You are awesome 🎉 Happy Birthday!";
+  }, 4000);
+
+  setTimeout(() => {
+    text.innerHTML = "Final surprise incoming... 💙";
+  }, 6000);
+}
+
+function wrongChoice() {
+  text.innerHTML = "Nope 😈 you can’t escape the surprise!";
+  
+  setTimeout(() => {
+    text.innerHTML = "Try again 😏";
+  }, 2000);
 }
